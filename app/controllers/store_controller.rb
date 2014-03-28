4e41bca582a67e3644e2a041cbb5aaf0ef026fee
Zdenko Koscina <zkoscina@gmail.com>
2014-03-28 18:40:49 -0300

    carro de compra

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,6 @@
 class StoreController < ApplicationController
   def index #definiendo el metodo index
 	 @products = Product.order(:tittle) #defino como variable una lista ordenadas de productos alfabeticamente según el titulo. 
-	 #notar que el metodo order no está definido, por tanto que está por defecto.
-	 @time = Time.now #defino una variable con la hora actual
+
   end
 end
